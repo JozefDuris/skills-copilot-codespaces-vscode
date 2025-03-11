8ff0e1e5203cca91bb89f69a27facbfeec645713
JozefDuris <j.duris@accenture.com>
2025-03-11 08:53:48 +0000

    added member.js

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+    const skills = [
+        {
+            id: 1,
+            name: 'HTML',
+            level: '80%',
+        },
+        {
+            id: 2,
+            name: 'CSS',
+            level: '70%',
+        },
+        {
+            id: 3,
+            name: 'Javascript',
+            level: '60%',
+        },
+        {
+            id: 4,
+            name: 'React',
+            level: '50%',
+        },
+    ];
+
+    return skills;
+}

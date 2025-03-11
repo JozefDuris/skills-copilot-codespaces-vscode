757cf0760dc1cfeab9d17d33cb31429083a24c7b
JozefDuris <j.duris@accenture.com>
2025-03-11 09:10:10 +0000

    update

--- comments.js
+++ comments.js
@@ -1,18 +1,45 @@
 //create web server
+//create an express app
+//create a router
+//create a route handler
+//listen on a port
+//create a form in the browser
+//submit the form
+//send the form data to the server
+//save the form data in a file
+//read the form data from the file
+//display the form data in the browser
+
 const express = require('express');
-const app = express();
-//parse incoming request
+const fs = require('fs');
 const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//create a new comment
-app.post('/comments', (req, res) => {
-  const { body } = req;
-  console.log(body);
-  res.json({
-    message: 'New comment created'
-  });
+
+const comments = [];
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
-//start the server
-app.listen(4001, () => {
-  console.log('Server is running');
+
+router.post('/comments', (req, res) => {
+  comments.push(req.body.comment);
+  fs.writeFileSync('comments.json', JSON.stringify(comments));
+  res.redirect('/comments');
+});
+
+router.get('/comments', (req, res) => {
+  const comments = JSON.parse(fs.readFileSync('comments.json'));
+  res.send(comments.map((comment, index) => `<p>${index + 1}: ${comment}</p>`).join(''));
+});
+
+app.use('/', router);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
